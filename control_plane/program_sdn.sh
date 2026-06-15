943a6d9a7eb509884745274ae4b6e01711f89f14
andreR3nner <oandrerenner@gmail.com>
2026-06-15 08:40:17 -0300

    fix: robust multicast node association to support full restarts

--- control_plane/program_sdn.sh
+++ control_plane/program_sdn.sh
@@ -19,10 +19,13 @@ run_cli() {
 
 # S1 Config: switch_id = 1, mcast_grp 1 = flood to ports 1, 2
 S1_COMMANDS="reset_state
-table_add node_info set_switch_id => 1
+table_set_default node_info set_switch_id 1
 mc_mgrp_create 1
 mc_node_create 1 1 2
+mc_node_associate 1 0
 mc_node_associate 1 1
+mc_node_associate 1 2
+mc_node_associate 1 3
 
 table_add ipv4_lpm ipv4_forward 10.0.0.2/32 => 00:00:00:00:02:02 00:aa:00:00:01:02 2
 table_add ipv4_lpm ipv4_forward 10.0.0.1/32 => 00:00:00:00:01:01 00:aa:00:00:01:01 1
@@ -30,10 +33,13 @@ table_add ipv4_lpm ipv4_forward 10.0.0.1/32 => 00:00:00:00:01:01 00:aa:00:00:01:
 
 # S2 Config: switch_id = 2, mcast_grp 1 = flood to ports 1, 2
 S2_COMMANDS="reset_state
-table_add node_info set_switch_id => 2
+table_set_default node_info set_switch_id 2
 mc_mgrp_create 1
 mc_node_create 2 1 2
+mc_node_associate 1 0
+mc_node_associate 1 1
 mc_node_associate 1 2
+mc_node_associate 1 3
 
 table_add ipv4_lpm ipv4_forward 10.0.0.2/32 => 00:00:00:00:02:02 00:aa:00:00:02:02 2
 table_add ipv4_lpm ipv4_forward 10.0.0.1/32 => 00:00:00:00:01:01 00:aa:00:00:02:01 1
